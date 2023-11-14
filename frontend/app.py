fdfee6e1ce4ffeed00c3edc3f8f9da2f514280ca
Logan Winnie <loganwinnier@gmail.com>
2023-11-14 17:43:46 -0500

    add class methods

--- frontend/app.py
+++ frontend/app.py
@@ -1,14 +1,27 @@
-from flask import Flask, request;
+import os
+from flask import Flask, request
+from dotenv import load_dotenv
+from models import db, connect_db, User, Space, Image
 
 app = Flask(__name__)
 
-@app.get('/users')
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "postgresql:///sharebnb"
+)
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+connect_db(app)
+
+
+@app.get("/users")
 def get_all_users():
-    '''Gets a list of all users'''
+    """Gets a list of all users"""
 
 
-@app.get('/user/:id')
+@app.get("/user/:id")
 def get_user():
-    '''Get data on one user'''
-
-
+    """Get data on one user"""
--- frontend/models.py
+++ frontend/models.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 db = SQLAlchemy()
 
 
@@ -34,6 +36,7 @@ class User(db.Model):
     email = db.Column(
         db.String(50),
         nullable=False,
+        unique=True,
     )
 
     password = db.Column(
@@ -47,7 +50,50 @@ class User(db.Model):
         nullable=False,
     )
 
-    listings = db.relationship("Space", secondary="users_spaces", backref="owner")
+    listings = db.relationship(
+        "Space",
+        secondary="users_spaces",
+        backref="owner",
+    )
+
+    @classmethod
+    def signup(cls, username, first_name, last_name, email, password):
+        """Sign up user.
+
+        Hashes password and adds user to db
+        """
+        hashed_pwd = bcrypt.generate_password_hash(password).decode("UTF-8")
+
+        user = User(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            password=hashed_pwd,
+        )
+
+        db.session.add(user)
+        return user
+
+    @classmethod
+    def authenticate(cls, username, password):
+        """Find user with `username` and `password`.
+
+        This is a class method (call it on the class, not an individual user.)
+        It searches for a user whose password hash matches this password
+        and, if it finds such a user, returns that user object.
+
+        If this can't find matching user (or if password is wrong), returns
+        False.
+        """
+        user = cls.query.filter_by(username=username).one_or_none()
+
+        if user:
+            is_auth = bcrypt.check_password_hash(user.password, password)
+            if is_auth:
+                return user
+
+        return False
 
 
 class Space(db.Model):
